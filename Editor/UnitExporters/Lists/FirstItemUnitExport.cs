18bc7f45cf228a4d98b3a51b3b25acaff16d22d9
Robert Dorn <dorn@prefrontalcortex.de>
2025-01-16 14:28:29 +0100

    added FirstItem unit exporter

---
+++ Editor/UnitExporters/Lists/FirstItemUnitExport.cs
@@ -0,0 +1,33 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityGLTF.Interactivity.Export;
+
+namespace Editor.UnitExporters.Lists
+{
+    public class FirstItemUnitExport : IUnitExporter
+    {
+        public Type unitType { get => typeof(FirstItem); }
+        
+        [InitializeOnLoadMethod]
+        private static void Register()
+        {
+            UnitExporterRegistry.RegisterExporter(new FirstItemUnitExport());
+        }
+        
+        public void InitializeInteractivityNodes(UnitExporter unitExporter)
+        {
+            var unit = unitExporter.unit as FirstItem;
+            var list = ListHelpers.FindListByConnections(unitExporter.exportContext, unit);
+            if (list == null)
+            {
+                Debug.LogError("Could not find list for FirstItem unit");
+                return;
+            }
+            
+            VariablesHelpers.GetVariable(unitExporter, list.StartIndex, out var startIndex);
+            startIndex.MapToPort(unit.firstItem);
+        }
+    }
+}

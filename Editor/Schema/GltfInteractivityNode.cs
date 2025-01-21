725d758bdfe98032925dca65872ce2da57996bc2
Robert Dorn <dorn@prefrontalcortex.de>
2025-01-21 15:35:09 +0100

    changed String and Bool Values to be also serialized as JArray

--- Editor/Schema/GltfInteractivityNode.cs
+++ Editor/Schema/GltfInteractivityNode.cs
@@ -264,64 +264,64 @@ namespace UnityGLTF.Interactivity
 
         public static class ValueSerializer
         {
-           public static void Serialize(object value, JObject valueObject)
+            public static void Serialize(object value, JObject valueObject)
             {
-                if (value != null)
+                if (value == null)
+                    return;
+                
+                if (value is Color color)
                 {
-                    if (value is Color color)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(color.r, color.g, color.b, color.a)));
-                    }
-                    else if (value is Color32 color32)
-                    {
-                        Color col = color32;
-                        valueObject.Add(new JProperty("value", new JArray(col.r, col.g, col.b, col.a)));
-                    }                  
-                    else if (value is Matrix4x4 m4)
-                    {
-                        // TODO check if this is the correct row-column order
-                        valueObject.Add(new JProperty("value", new JArray(
-                            m4.m00, m4.m01, m4.m02, m4.m03,
-                            m4.m10, m4.m11, m4.m12, m4.m13,
-                            m4.m20, m4.m21, m4.m22, m4.m23,
-                            m4.m30, m4.m31, m4.m32, m4.m33)));
-                    }
-                    else if (value is Vector4 v4)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(v4.x, v4.y, v4.z, v4.w)));
-                    }
-                    else if (value is Vector3 v3)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(v3.x, v3.y, v3.z)));
-                    }
-                    else if (value is Vector2 v2)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(v2.x, v2.y)));
-                    }
-                    else if (value is Quaternion q)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(q.x, q.y, q.z, q.w)));
-                    }
-                    else if (value is bool b)
-                    {
-                        valueObject.Add(new JProperty("value", b));
-                    }
-                    else if (value is string s)
-                    {
-                        valueObject.Add(new JProperty("value", s));
-                    }
-                    else if (value is int i)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(i)));
-                    }
-                    else if (value is float f)
-                    {
-                        valueObject.Add(new JProperty("value", new JArray(f)));
-                    }
-                    else
-                    {
-                        valueObject.Add(new JProperty("value", value));
-                    }
+                    valueObject.Add(new JProperty("value", new JArray(color.r, color.g, color.b, color.a)));
+                }
+                else if (value is Color32 color32)
+                {
+                    Color col = color32;
+                    valueObject.Add(new JProperty("value", new JArray(col.r, col.g, col.b, col.a)));
+                }
+                else if (value is Matrix4x4 m4)
+                {
+                    // TODO check if this is the correct row-column order
+                    valueObject.Add(new JProperty("value", new JArray(
+                        m4.m00, m4.m01, m4.m02, m4.m03,
+                        m4.m10, m4.m11, m4.m12, m4.m13,
+                        m4.m20, m4.m21, m4.m22, m4.m23,
+                        m4.m30, m4.m31, m4.m32, m4.m33)));
+                }
+                else if (value is Vector4 v4)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(v4.x, v4.y, v4.z, v4.w)));
+                }
+                else if (value is Vector3 v3)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(v3.x, v3.y, v3.z)));
+                }
+                else if (value is Vector2 v2)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(v2.x, v2.y)));
+                }
+                else if (value is Quaternion q)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(q.x, q.y, q.z, q.w)));
+                }
+                else if (value is bool b)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(b)));
+                }
+                else if (value is string s)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(s)));
+                }
+                else if (value is int i)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(i)));
+                }
+                else if (value is float f)
+                {
+                    valueObject.Add(new JProperty("value", new JArray(f)));
+                }
+                else
+                {
+                    valueObject.Add(new JProperty("value", new JArray(value)));
                 }
             }    
         }

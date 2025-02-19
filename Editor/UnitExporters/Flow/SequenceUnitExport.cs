6a1a7c46f623b258149f4758e42018f33e05ee0d
Robert Dorn <dorn@prefrontalcortex.de>
2025-02-19 18:51:30 +0100

    added coroutine support to Sequence (WIP)

--- Editor/UnitExporters/Flow/SequenceUnitExport.cs
+++ Editor/UnitExporters/Flow/SequenceUnitExport.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Editor.UnitExporters.Helpers;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityGLTF.Interactivity.Schema;
 
 namespace UnityGLTF.Interactivity.Export
 {
-    public class SequenceUnitExport : IUnitExporter
+    public class SequenceUnitExport : IUnitExporter, ICoroutineAwaiter, ICoroutineWait
     {
         public Type unitType { get => typeof(Sequence); }
         
@@ -19,29 +21,58 @@ namespace UnityGLTF.Interactivity.Export
         public bool InitializeInteractivityNodes(UnitExporter unitExporter)
         {
             var unit = unitExporter.unit as Sequence;
-            var node = unitExporter.CreateNode(new Flow_SequenceNode());
+            var validOutputs = unit.multiOutputs.Where( output => output.connection != null && output.connection.destination != null).ToList();
             
-            unitExporter.MapInputPortToSocketName(unit.enter, Flow_SequenceNode.IdFlowIn, node);
+            GltfInteractivityUnitExporterNode node;
+            // node = unitExporter.CreateNode(new Flow_SequenceNode());
+            // unitExporter.MapInputPortToSocketName(unit.enter, Flow_SequenceNode.IdFlowIn, node);
+            // if (unit.multiOutputs.Count > 0)
+            // {
+            //     int index = 0;
+            //     foreach (var output in validOutputs)
+            //     {
+            //         var id = "sequ" + index.ToString("D3");
+            //         node.FlowOut(id).MapToControlOutput(output);
+            //         index++;
+            //     }
+            // }
             
+            node = unitExporter.CreateNode(new Flow_SequenceNode());
+            node.FlowIn(Flow_SequenceNode.IdFlowIn).MapToControlInput(unit.enter);
+            
+            var multiGate = unitExporter.CreateNode(new Flow_MultiGateNode());
+            multiGate.ConfigurationData[Flow_MultiGateNode.IdConfigIsLoop].Value = false;
+            multiGate.ConfigurationData[Flow_MultiGateNode.IdConfigIsRandom].Value = false;
+            
+            node.FlowOut("a").ConnectToFlowDestination(multiGate.FlowIn(Flow_MultiGateNode.IdFlowInReset));
+            node.FlowOut("b").ConnectToFlowDestination(multiGate.FlowIn(Flow_MultiGateNode.IdFlowIn));
+                
             if (unit.multiOutputs.Count > 0)
             {
                 int index = 0;
-                foreach (var output in unit.multiOutputs)
+                foreach (var output in validOutputs)
                 {
-                    if (output.connection == null || output.connection.destination == null)
-                        continue;
-                    
-                    var n = new GltfInteractivityUnitExporterNode.FlowSocketData();
-                    var desc = new GltfInteractivityNodeSchema.FlowSocketDescriptor();
-                    var id = "sequence" + index.ToString("D3");
-                    
-                    node.FlowSocketConnectionData.Add(id, n);
-                    unitExporter.MapOutFlowConnectionWhenValid(output, id, node);
-                    
+                    multiGate.FlowOut(index.ToString()).MapToControlOutput(output);
+                    var awaiter = CoroutineHelper.AddCoroutineAwaiter(unitExporter, index.ToString());
+                    awaiter.FlowOutDoneSocket().ConnectToFlowDestination(multiGate.FlowIn(Flow_MultiGateNode.IdFlowIn));
                     index++;
                 }
             }
+            
+            unitExporter.exportContext.OnNodesCreated += (nodes) =>
+            {
+                var awaiter = CoroutineHelper.FindCoroutineAwaiter(unitExporter, node);
+                if (awaiter == null)
+                    return;
+
+                var nextIndex = multiGate.FlowSocketConnectionData.Count;
+                
+                var newFlowOut = multiGate.FlowOut(nextIndex.ToString());
+                awaiter.AddCoroutineWait(unitExporter, multiGate, newFlowOut.socket.Key);
+            };
+            
             return true;
         }
+
     }
 }

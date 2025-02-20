6c1004db0d8971120252f80ae9ee3653e5548c21
Robert Dorn <dorn@prefrontalcortex.de>
2025-02-20 12:01:29 +0100

    added coroutine support to WhileLoop exporter

--- Editor/UnitExporters/Flow/WhileLoopUnitExport.cs
+++ Editor/UnitExporters/Flow/WhileLoopUnitExport.cs
@@ -5,7 +5,7 @@ using UnityGLTF.Interactivity.Schema;
 
 namespace UnityGLTF.Interactivity.Export
 {
-    public class WhileLoopUnitExport : IUnitExporter
+    public class WhileLoopUnitExport : IUnitExporter, ICoroutineAwaiter, ICoroutineWait
     {
         public Type unitType
         {
@@ -21,11 +21,38 @@ namespace UnityGLTF.Interactivity.Export
         public bool InitializeInteractivityNodes(UnitExporter unitExporter)
         {
             var unit = unitExporter.unit as While;
-            var node = unitExporter.CreateNode(new Flow_WhileNode());
+            
+            var coroutine = CoroutineHelper.CoroutineRequired(unit);
 
-            unitExporter.MapInputPortToSocketName(unit.condition, Flow_WhileNode.IdCondition, node);
-            unitExporter.MapOutFlowConnectionWhenValid(unit.body, Flow_WhileNode.IdLoopBody, node);
-            unitExporter.MapOutFlowConnectionWhenValid(unit.exit, Flow_WhileNode.IdCompleted, node);
+            if (!coroutine)
+            {
+                var node = unitExporter.CreateNode(new Flow_WhileNode());
+
+                unitExporter.MapInputPortToSocketName(unit.condition, Flow_WhileNode.IdCondition, node);
+                unitExporter.MapOutFlowConnectionWhenValid(unit.body, Flow_WhileNode.IdLoopBody, node);
+                unitExporter.MapOutFlowConnectionWhenValid(unit.exit, Flow_WhileNode.IdCompleted, node);
+            }
+            else
+            {
+                var branch  = unitExporter.CreateNode(new Flow_BranchNode());
+                branch.FlowIn(Flow_BranchNode.IdFlowIn).MapToControlInput(unit.enter); 
+                branch.FlowOut(Flow_BranchNode.IdFlowOutFalse).MapToControlOutput(unit.exit);
+                branch.FlowOut(Flow_BranchNode.IdFlowOutTrue).MapToControlOutput(unit.body);
+                branch.ValueIn(Flow_BranchNode.IdCondition).MapToInputPort(unit.condition);
+                
+                var awaiter = CoroutineHelper.AddCoroutineAwaiter(unitExporter, Flow_BranchNode.IdFlowOutTrue);
+                awaiter.FlowOutDoneSocket().ConnectToFlowDestination(branch.FlowIn(Flow_BranchNode.IdFlowIn));
+                    
+                unitExporter.exportContext.OnNodesCreated += (nodes) =>
+                {
+                    var awaiter = CoroutineHelper.FindCoroutineAwaiter(unitExporter, branch);
+                    if (awaiter == null)
+                        return;
+                    awaiter.AddCoroutineWait(unitExporter, branch, Flow_BranchNode.IdFlowOutFalse);
+                };
+            }
+
+            
             return true;
         }
     }
